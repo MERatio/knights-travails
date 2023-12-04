aa2f30655c65763ea1a9a723b9ab068d41e4a5ab
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-12-04 12:38:09 +0800

    Add a func that gets the fastest path

---
+++ src/index.js
@@ -0,0 +1,79 @@
+"use strict";
+
+function copy2dArray(originalArray) {
+  return originalArray.map((row) => row.slice());
+}
+
+function getPossibleNextMove(startCoor) {
+  const [row, column] = startCoor;
+  const possibleNextMove = [];
+  if (row <= 5 && column <= 6) {
+    possibleNextMove.push([row + 2, column + 1]);
+  }
+  if (row <= 6 && column <= 5) {
+    possibleNextMove.push([row + 1, column + 2]);
+  }
+  if (row >= 1 && column <= 5) {
+    possibleNextMove.push([row - 1, column + 2]);
+  }
+  if (row >= 2 && column <= 6) {
+    possibleNextMove.push([row - 2, column + 1]);
+  }
+  if (row >= 2 && column >= 1) {
+    possibleNextMove.push([row - 2, column - 1]);
+  }
+  if (row >= 1 && column >= 2) {
+    possibleNextMove.push([row - 1, column - 2]);
+  }
+  if (row <= 6 && column >= 2) {
+    possibleNextMove.push([row + 1, column - 2]);
+  }
+  if (row <= 5 && column >= 1) {
+    possibleNextMove.push([row + 2, column - 1]);
+  }
+  return possibleNextMove;
+}
+
+function isArrayEqual(array1, array2) {
+  return array1.every((el, idx) => el === array2[idx]);
+}
+
+function getFastestPath(startCoor, targetCoor) {
+  const queue = [
+    {
+      board: new Array(8).fill().map(() => new Array(8).fill(0)),
+      coor: startCoor,
+      moves: [startCoor],
+    },
+  ];
+
+  queue[0].board[startCoor[0]][startCoor[1]] = 1;
+
+  while (queue.length > 0) {
+    const cur = queue.shift();
+    if (isArrayEqual(cur.coor, targetCoor)) {
+      return cur.moves;
+    }
+    const possibleNextMoves = getPossibleNextMove(cur.coor);
+    for (const possibleNextMove of possibleNextMoves) {
+      const isVisited =
+        cur.board[possibleNextMove[0]][possibleNextMove[1]] === 1;
+      if (!isVisited) {
+        const newBoard = copy2dArray(cur.board);
+        newBoard[possibleNextMove[0]][possibleNextMove[1]] = 1;
+        queue.push({
+          board: newBoard,
+          coor: possibleNextMove,
+          moves: [...cur.moves, possibleNextMove],
+        });
+      }
+    }
+  }
+}
+
+function knightMoves(startCoor, targetCoor) {
+  const fastestPath = getFastestPath(startCoor, targetCoor);
+  return fastestPath;
+}
+
+console.log(knightMoves([3, 3], [4, 3]));
